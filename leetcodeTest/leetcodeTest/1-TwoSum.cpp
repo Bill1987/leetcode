565f6e51977df905cf7ac3f641cceddf1d2e413c
Bill <qimingzhang1987@gmail.com>
2022-04-05 16:24:24 +1200

    创建 LeetCode代码工程
    Signed-off-by: Bill <qimingzhang1987@gmail.com>

---
+++ leetcodeTest/leetcodeTest/1-TwoSum.cpp
@@ -0,0 +1,58 @@
+/*
+* 1. Two Sum   Easy
+* 
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+* 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+* 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+* You can return the answer in any order.
+* 你可以按任意顺序返回答案。
+* 
+* 
+* Example 1:
+* Input: nums = [2,7,11,15], target = 9
+* Output: [0,1]
+* Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+* 
+* 
+* Example 2:
+* Input: nums = [3,2,4], target = 6
+* Output: [1,2]
+* 
+* 
+* Example 3:
+* Input: nums = [3,3], target = 6
+* Output: [0,1]
+* 
+* 
+* Constraints:
+* 2 <= nums.length <= 104
+* -109 <= nums[i] <= 109
+* -109 <= target <= 109
+* Only one valid answer exists.
+* 只会存在一个有效答案
+* 
+* Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+* 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+* */
+
+
+
+#include <ostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+
+
+int main()
+{
+	return 0;
+}
