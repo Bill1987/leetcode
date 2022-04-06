d90424b84b1cca37b521fb4588e0fff5fff8ff2e
Bill <qimingzhang1987@gmail.com>
2022-04-06 15:31:43 +1200

    提交 练习1 twoSum
    Signed-off-by: Bill <qimingzhang1987@gmail.com>

--- leetcodeTest/leetcodeTest/1-TwoSum.cpp
+++ leetcodeTest/leetcodeTest/1-TwoSum.cpp
@@ -38,21 +38,107 @@
 
 
 
-#include <ostream>
+#include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    /*
+    * 暴力双循环
+    * 此方法，好理解，但时间复杂度为 O(n2)  空间复杂度O(1)
+    *
+    * 执行结果：通过
+    * 执行用时：300 ms, 在所有 C++ 提交中击败了 30.18%的用户
+    * 内存消耗：10 MB, 在所有 C++ 提交中击败了 63.91%的用户
+    * 通过测试用例：57 / 57
+    *
+    * Success
+    * Runtime: 606 ms, faster than 19.05% of C++ online submissions for Two Sum.
+    * Memory Usage: 10.3 MB, less than 66.99% of C++ online submissions for Two Sum.
+    */
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
 
+        // 循环全部数据，设为第一个数
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // 在第一个数之后，寻找第二个数字
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                // 找到和为target的两个数字
+                if ( target == nums[i] + nums[j])
+                {
+                    // 返回两个数的位置
+                    return {i, j};
+                }
+            }
+        }
+        // 没有找到返回位置-1
+        return { -1, -1 };
     }
+
+
+    /*
+    * 使用hash 的map
+    * 时间复杂度 O(n)  空间复杂度O(n)
+    * 
+    * 执行结果：通过
+    * 执行用时：8 ms, 在所有 C++ 提交中击败了92.14%的用户
+    * 内存消耗：10.8 MB, 在所有 C++ 提交中击败了15.81%的用户
+    * 通过测试用例：57 / 57
+    * 
+    * Success
+    * Runtime: 4 ms, faster than 99.30% of C++ online submissions for Two Sum.
+    * Memory Usage: 11.2 MB, less than 23.64% of C++ online submissions for Two Sum.
+    */
+    vector<int> twoSum_2(vector<int>& nums, int target)
+    {
+        // 第一个元素是当前数字，第二个元素是位置
+        map<int, int> mNums;
+
+        // 循环全部数据，
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // 查找map中是否有需要的第二个数字
+            int nSecond = target - nums[i];
+            if (mNums.count(nSecond) > 0)
+            {
+                // 返回两个数据的位置
+                return {mNums[nSecond], i };
+            }
+
+            // 把当前数据和位置插入到map中
+            mNums[nums[i]] = i;
+        }
+
+        // 没有找到返回位置-1
+        return { -1, -1 };
+    }
+
 };
 
 
 int main()
 {
+    Solution sol;
+    vector<int> vData;
+    vector<int> vRes;
+
+    vData = {2, 7, 11, 15};
+    vRes = sol.twoSum_2(vData, 9);
+    cout << "[" << vRes[0] << ", " << vRes[1] << "]" << endl;
+
+    vData = { 3,2,4 };
+    vRes = sol.twoSum_2(vData, 6);
+    cout << "[" << vRes[0] << ", " << vRes[1] << "]" << endl;
+
+    vData = { 3,3 };
+    vRes = sol.twoSum_2(vData, 6);
+    cout << "[" << vRes[0] << ", " << vRes[1] << "]" << endl;
+
 	return 0;
 }
